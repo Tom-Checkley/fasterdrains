bb2828f1d0c812bf596687e9fe9a51fb62398e64
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2017-12-26 16:05:44 +0000

    test

--- app/index.ts
+++ app/index.ts
@@ -14,10 +14,10 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'dist/public'));
 
 app.get('*', (req: any, res: any) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + 'dist/public/index.html'));
 });
 
 app.listen(port, () => {
